5e9dedd055407048b451b3ec78df4595b1e65ba6
morris <1316411382@qq.com>
2020-07-27 16:34:57 +0800

    Initial commit

---
+++ src/main/java/web/load_balance/LoadBalanceAlgorithm.java
@@ -0,0 +1,16 @@
+package web.load_balance;
+
+/**
+ * @Author: morris
+ * @Date: 2020/7/27 16:31
+ * @description
+ * @reviewer
+ */
+public class LoadBalanceAlgorithm {
+    public static void main(String[] args) {
+
+    }
+
+
+    
+}
